f1a35169fa9b3550d34a08ab79485229630d23e8
Yuzhe Yang <1400012996@pku.edu.cn>
2017-06-21 20:33:13 +0800

    Update OFDM Simulation, using QPSK/BPSK
    by comparing their BER.

--- OFDM_Qpsk_Bpsk.m
+++ OFDM_Qpsk_Bpsk.m
@@ -1,64 +1,63 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%  ¶ÔË¼¿¼Ìâ2×÷³ö·ÂÕæÀ´ÑéÖ¤½áÂÛµÄÕıÈ·ĞÔ                                                           %%
-%  ÏµÍ³´ø¿í3M£¬128µãIFFT£¬CP³¤¶ÈÎª32£¬ÊÔ·ÖÎöµ±²ÉÓÃQPSKµ÷ÖÆÊ±£¬µÍÍ¨ÂË²¨Æ÷µÄ²ÎÊıÊÇ·ñĞèÒª¸Ä±ä£¿ÎªÊ²Ã´£¿%%
+%  å¯¹æ€è€ƒé¢˜2ä½œå‡ºä»¿çœŸæ¥éªŒè¯ç»“è®ºçš„æ­£ç¡®æ€§                                                           %%
+%  ç³»ç»Ÿå¸¦å®½3Mï¼Œ128ç‚¹IFFTï¼ŒCPé•¿åº¦ä¸º32ï¼Œè¯•åˆ†æå½“é‡‡ç”¨QPSKè°ƒåˆ¶æ—¶ï¼Œä½é€šæ»¤æ³¢å™¨çš„å‚æ•°æ˜¯å¦éœ€è¦æ”¹å˜ï¼Ÿä¸ºä»€ä¹ˆï¼Ÿ%%
 %                                                                                             %%
-%  ÊÕ·¢¶Ë£ºFIRÂË²¨Æ÷£¬µÈ²¨ÎÆÂË²¨Æ÷£¨ÓëÖ®Ç°µÄÂË²¨Æ÷ÏàÍ¬£©                                          %%
-%  ĞÅµÀ£ºAWGNĞÅµÀ                                                                              %%
-%  OFDM for QPSK£ºSubcarriers ·Ö±ğÎª 64 ºÍ 128                                                 %%
+%  æ”¶å‘ç«¯ï¼šFIRæ»¤æ³¢å™¨ï¼Œç­‰æ³¢çº¹æ»¤æ³¢å™¨ï¼ˆä¸ä¹‹å‰çš„æ»¤æ³¢å™¨ç›¸åŒï¼‰                                          %%
+%  ä¿¡é“ï¼šAWGNä¿¡é“                                                                              %%
+%  OFDM for QPSKï¼šSubcarriers åˆ†åˆ«ä¸º 64 å’Œ 128                                                 %%
 %                                                                                             %%
-%                                             ÑîÓî†´                                           %%
-%                                           1400012996                                        %%
+%                                         Yuzhe Yang                                          %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear all
 
-%---------------------------------------- ÏµÍ³²ÎÊı¶¨Òå ----------------------------------------%
-nFFT = 128;             % nµãfftµÄ³¤¶È
-nBpsk_Ofdm = 128;       % Ã¿¸öOFDM·ûºÅ´øÓĞµÄBPSK·ûºÅ
-nBit_Sym = 128;         % Ã¿¸öOFDM·ûºÅËù´øµÄbitÊıÄ¿£¨¶ÔÓÚBPSK¶øÑÔºÍ nBpsk_Ofdm Ò»Ñù£©
-nSym = 10^4;            % OFDM ·ûºÅÊıÄ¿£¨bit number > 10^5£¬¼´symbol > 10^3£¬´Ë´¦È¡10^4±£Ö¤ÊıÄ¿¾«È·¶È£©
+%---------------------------------------- ç³»ç»Ÿå‚æ•°å®šä¹‰ ----------------------------------------%
+nFFT = 128;             % nç‚¹fftçš„é•¿åº¦
+nBpsk_Ofdm = 128;       % æ¯ä¸ªOFDMç¬¦å·å¸¦æœ‰çš„BPSKç¬¦å·
+nBit_Sym = 128;         % æ¯ä¸ªOFDMç¬¦å·æ‰€å¸¦çš„bitæ•°ç›®ï¼ˆå¯¹äºBPSKè€Œè¨€å’Œ nBpsk_Ofdm ä¸€æ ·ï¼‰
+nSym = 10^4;            % OFDM ç¬¦å·æ•°ç›®ï¼ˆbit number > 10^5ï¼Œå³symbol > 10^3ï¼Œæ­¤å¤„å–10^4ä¿è¯æ•°ç›®ç²¾ç¡®åº¦ï¼‰
 
 EbN0dB = 0:0.5:12;      % Bit to Noise Ratio
 EsN0dB = EbN0dB + 10*log10(nBpsk_Ofdm/nFFT) - 10*log10(8);         % BPSK
 EsN0dB2 = EbN0dB + 10*log10(2) - 10*log10(8);                      % QPSK
 
-%---------------------------------------- ÂË²¨Æ÷Éè¼Æ ¼° Æä·ùÏàÆµÏìÓ¦ ----------------------------------------%
+%---------------------------------------- æ»¤æ³¢å™¨è®¾è®¡ åŠ å…¶å¹…ç›¸é¢‘å“åº” ----------------------------------------%
 [n,f,a,w] = firpmord([3.5*10^6 4.5*10^6], [1, 0], [0.005, 0.001], 24*10^6);
-Hd = firpm(n,f,a,w);                        % Éè¼ÆÂË²¨Æ÷µÄÏµÊı
+Hd = firpm(n,f,a,w);                        % è®¾è®¡æ»¤æ³¢å™¨çš„ç³»æ•°
 
 
-%---------------------------------------- OFDM ÏµÍ³Îó±ÈÌØÂÊ·ÂÕæ ----------------------------------------%
+%---------------------------------------- OFDM ç³»ç»Ÿè¯¯æ¯”ç‰¹ç‡ä»¿çœŸ ----------------------------------------%
 for ii = 1:length(EbN0dB)
 
-    % ·¢Éä¶Ë
-    s_bit = round(rand(1,nBit_Sym*nSym));               % Ëæ»ú0, 1±ÈÌØĞòÁĞ
-    s_bit_bpsk = 2*s_bit - 1;                           % BPSKµ÷ÖÆ£º 0 --> -1, 1 --> +1
-    s_bit_bpsk = reshape(s_bit_bpsk,nBit_Sym,nSym).';   % ½«µ÷ÖÆºóĞÅºÅÖØĞÂ·Ö×éÎª nSym*nBit_Sym ¾ØÕó£¬±ãÓÚ²Ù×÷
+    % å‘å°„ç«¯
+    s_bit = round(rand(1,nBit_Sym*nSym));               % éšæœº0, 1æ¯”ç‰¹åºåˆ—
+    s_bit_bpsk = 2*s_bit - 1;                           % BPSKè°ƒåˆ¶ï¼š 0 --> -1, 1 --> +1
+    s_bit_bpsk = reshape(s_bit_bpsk,nBit_Sym,nSym).';   % å°†è°ƒåˆ¶åä¿¡å·é‡æ–°åˆ†ç»„ä¸º nSym*nBit_Sym çŸ©é˜µï¼Œä¾¿äºæ“ä½œ
     
     I_bit = 2*(s_bit(1:2:end) - 0.5);
     Q_bit = 2*(s_bit(2:2:end) - 0.5);
-    s_bit_qpsk = I_bit + 1j*Q_bit;                      % QPSKµ÷ÖÆ£º1+j, 1-j, -1+j, -1-j
+    s_bit_qpsk = I_bit + 1j*Q_bit;                      % QPSKè°ƒåˆ¶ï¼š1+j, 1-j, -1+j, -1-j
     s_bit_qpsk = reshape(s_bit_qpsk,nBit_Sym,nSym/2).';
     
-    % IFFT±ä»» ÊµÏÖOFDMÃ¿¸ö·ûºÅ
+    % IFFTå˜æ¢ å®ç°OFDMæ¯ä¸ªç¬¦å·
     s_ofdm_bpsk = (nFFT/sqrt(nBpsk_Ofdm))*ifft(fftshift(s_bit_bpsk.')).';       % BPSK
     s_ofdm_qpsk = (nFFT/sqrt(nBpsk_Ofdm/2))*ifft(fftshift(s_bit_qpsk.')).';     % QPSK
 
-    % ²åÈëÑ­»·Ç°×ºCP£¬Éè¶¨Æä³¤¶ÈÎª L=32
+    % æ’å…¥å¾ªç¯å‰ç¼€CPï¼Œè®¾å®šå…¶é•¿åº¦ä¸º L=32
     s_cp_bpsk = [s_ofdm_bpsk(:,97:128) s_ofdm_bpsk];            % BPSK
     s_cp_qpsk = [s_ofdm_qpsk(:,97:128) s_ofdm_qpsk];            % QPSK
 
-    % ½«¶à¸öOFDM·ûºÅ´®Áªµ½Ò»¸öÒ»Î¬ÏòÁ¿
+    % å°†å¤šä¸ªOFDMç¬¦å·ä¸²è”åˆ°ä¸€ä¸ªä¸€ç»´å‘é‡
     s_cp_bpsk = reshape(s_cp_bpsk.',1,nSym*(32+128));           % BPSK
     s_cp_qpsk = reshape(s_cp_qpsk.',1,nSym/2*(32+128));         % QPSK
     
-    % ½«Êµ²¿ÓëĞé²¿ĞÅºÅ·Ö±ğ´¦Àí
+    % å°†å®éƒ¨ä¸è™šéƒ¨ä¿¡å·åˆ†åˆ«å¤„ç†
     s_I_bpsk = real(s_cp_bpsk);
     s_Q_bpsk = imag(s_cp_bpsk);
     s_I_qpsk = real(s_cp_qpsk);
     s_Q_qpsk = imag(s_cp_qpsk);
     
-    % 8±¶ÉÏ²ÉÑù
+    % 8å€ä¸Šé‡‡æ ·
     s_Iup_bpsk = upsample(s_I_bpsk,8);
     s_Qup_bpsk = upsample(s_Q_bpsk,8);
     s_Iup_bpsk = reshape(s_Iup_bpsk, 8*(nBit_Sym+32), nSym).';      % reshape
@@ -69,22 +68,22 @@ for ii = 1:length(EbN0dB)
     s_Iup_qpsk = reshape(s_Iup_qpsk, 8*(nBit_Sym+32), nSym/2).';      % reshape
     s_Qup_qpsk = reshape(s_Qup_qpsk, 8*(nBit_Sym+32), nSym/2).';
     
-    % ¾­¹ı·¢Éä¶ËµÍÍ¨ÂË²¨Æ÷
+    % ç»è¿‡å‘å°„ç«¯ä½é€šæ»¤æ³¢å™¨
     s_I_filter_bpsk = zeros(nSym,160*8 + n);
     s_Q_filter_bpsk = zeros(nSym,160*8 + n);
     for j = 1:nSym
-        s_I_filter_bpsk(j,:) = conv(s_Iup_bpsk(j,:),Hd);                  % conv¾í»ı
+        s_I_filter_bpsk(j,:) = conv(s_Iup_bpsk(j,:),Hd);                  % convå·ç§¯
         s_Q_filter_bpsk(j,:) = conv(s_Qup_bpsk(j,:),Hd);
     end
     
     s_I_filter_qpsk = zeros(nSym/2,160*8 + n);
     s_Q_filter_qpsk = zeros(nSym/2,160*8 + n);
     for j = 1:nSym/2
-        s_I_filter_qpsk(j,:) = conv(s_Iup_qpsk(j,:),Hd);                  % conv¾í»ı
+        s_I_filter_qpsk(j,:) = conv(s_Iup_qpsk(j,:),Hd);                  % convå·ç§¯
         s_Q_filter_qpsk(j,:) = conv(s_Qup_qpsk(j,:),Hd);
     end
     
-    % ÓÉÓÚ¾í»ıÒıÈëÁË¶àÓàÏî£¬ĞèÒªÈ¥µô£¨ÂË²¨Æ÷½×Êı = µãÊı = n£©
+    % ç”±äºå·ç§¯å¼•å…¥äº†å¤šä½™é¡¹ï¼Œéœ€è¦å»æ‰ï¼ˆæ»¤æ³¢å™¨é˜¶æ•° = ç‚¹æ•° = nï¼‰
     s_I_filter_bpsk = s_I_filter_bpsk(:,(n/2+1):end-n/2);
     s_Q_filter_bpsk = s_Q_filter_bpsk(:,(n/2+1):end-n/2);
     s_I_filter_bpsk = reshape(s_I_filter_bpsk.', 1, 8*nSym*(32+128));     % reshape
@@ -95,15 +94,15 @@ for ii = 1:length(EbN0dB)
     s_I_filter_qpsk = reshape(s_I_filter_qpsk.', 1, 8*nSym/2*(32+128));     % reshape
     s_Q_filter_qpsk = reshape(s_Q_filter_qpsk.', 1, 8*nSym/2*(32+128));
     
-    % Á½Â·Õı½»ĞÅºÅ¾­¹ıLPFºóºÏ²¢
+    % ä¸¤è·¯æ­£äº¤ä¿¡å·ç»è¿‡LPFååˆå¹¶
     s_filter_bpsk = s_I_filter_bpsk + 1j*s_Q_filter_bpsk;
     s_filter_qpsk = s_I_filter_qpsk + 1j*s_Q_filter_qpsk;
     
-    % Í¨¹ıAWGNĞÅµÀ
+    % é€šè¿‡AWGNä¿¡é“
     s_awgn_bpsk = awgn(s_filter_bpsk, EsN0dB(ii), 'measured');
     s_awgn_qpsk = awgn(s_filter_qpsk, EsN0dB2(ii), 'measured');
     
-    % ½ÓÊÕ¶Ë½«Êµ²¿ÓëĞé²¿ĞÅºÅ·Ö±ğ´¦Àí
+    % æ¥æ”¶ç«¯å°†å®éƒ¨ä¸è™šéƒ¨ä¿¡å·åˆ†åˆ«å¤„ç†
     r_I_bpsk = real(s_awgn_bpsk);
     r_Q_bpsk = imag(s_awgn_bpsk);
     r_I_bpsk = reshape(r_I_bpsk, 8*(nBit_Sym+32), nSym).';        % reshape
@@ -114,22 +113,22 @@ for ii = 1:length(EbN0dB)
     r_I_qpsk = reshape(r_I_qpsk, 8*(nBit_Sym+32), nSym/2).';        % reshape
     r_Q_qpsk = reshape(r_Q_qpsk, 8*(nBit_Sym+32), nSym/2).';
     
-    % ¾­¹ı½ÓÊÕ¶ËµÍÍ¨ÂË²¨Æ÷
+    % ç»è¿‡æ¥æ”¶ç«¯ä½é€šæ»¤æ³¢å™¨
     r_I_filter_bpsk = zeros(nSym,160*8 + n);
     r_Q_filter_bpsk = zeros(nSym,160*8 + n);
     for j = 1:nSym
-        r_I_filter_bpsk(j,:) = conv(r_I_bpsk(j,:),Hd);            % conv¾í»ı
+        r_I_filter_bpsk(j,:) = conv(r_I_bpsk(j,:),Hd);            % convå·ç§¯
         r_Q_filter_bpsk(j,:) = conv(r_Q_bpsk(j,:),Hd);
     end
     
     r_I_filter_qpsk = zeros(nSym/2,160*8 + n);
     r_Q_filter_qpsk = zeros(nSym/2,160*8 + n);
     for j = 1:nSym/2
-        r_I_filter_qpsk(j,:) = conv(r_I_qpsk(j,:),Hd);            % conv¾í»ı
+        r_I_filter_qpsk(j,:) = conv(r_I_qpsk(j,:),Hd);            % convå·ç§¯
         r_Q_filter_qpsk(j,:) = conv(r_Q_qpsk(j,:),Hd);
     end
     
-    % ÓÉÓÚ¾í»ıÒıÈëÁË¶àÓàÏî£¬ĞèÒªÈ¥µô£¨ÂË²¨Æ÷½×Êı = µãÊı = n£©
+    % ç”±äºå·ç§¯å¼•å…¥äº†å¤šä½™é¡¹ï¼Œéœ€è¦å»æ‰ï¼ˆæ»¤æ³¢å™¨é˜¶æ•° = ç‚¹æ•° = nï¼‰
     r_I_filter_bpsk = r_I_filter_bpsk(:,(n/2+1):end-n/2);
     r_Q_filter_bpsk = r_Q_filter_bpsk(:,(n/2+1):end-n/2);
     r_I_filter_bpsk = reshape(r_I_filter_bpsk.', 1, 8*nSym*(32+128));     % reshape
@@ -140,36 +139,36 @@ for ii = 1:length(EbN0dB)
     r_I_filter_qpsk = reshape(r_I_filter_qpsk.', 1, 8*nSym/2*(32+128));     % reshape
     r_Q_filter_qpsk = reshape(r_Q_filter_qpsk.', 1, 8*nSym/2*(32+128));
     
-    % 8±¶ÏÂ²ÉÑù
+    % 8å€ä¸‹é‡‡æ ·
     r_Idown_bpsk = downsample(r_I_filter_bpsk,8);
     r_Qdown_bpsk = downsample(r_Q_filter_bpsk,8);
     
     r_Idown_qpsk = downsample(r_I_filter_qpsk,8);
     r_Qdown_qpsk = downsample(r_Q_filter_qpsk,8);
     
-    % Á½Â·Õı½»ĞÅºÅºÏ²¢
+    % ä¸¤è·¯æ­£äº¤ä¿¡å·åˆå¹¶
     r_filter_bpsk = r_Idown_bpsk + 1j*r_Qdown_bpsk;
-    r_cp_bpsk = reshape(r_filter_bpsk,160,nSym).';        % ½«µ÷ÖÆºóĞÅºÅÖØĞÂ·Ö×éÎª nSym*(nBit_Sym+L) ¾ØÕó£¬±ãÓÚ²Ù×÷
+    r_cp_bpsk = reshape(r_filter_bpsk,160,nSym).';        % å°†è°ƒåˆ¶åä¿¡å·é‡æ–°åˆ†ç»„ä¸º nSym*(nBit_Sym+L) çŸ©é˜µï¼Œä¾¿äºæ“ä½œ
     
     r_filter_qpsk = r_Idown_qpsk + 1j*r_Qdown_qpsk;
     r_cp_qpsk = reshape(r_filter_qpsk,160,nSym/2).';
     
-    % ¶ªÆúÑ­»·Ç°×ºCP£¬L=32
+    % ä¸¢å¼ƒå¾ªç¯å‰ç¼€CPï¼ŒL=32
     r_fft_bpsk = r_cp_bpsk(:,33:end);
     r_fft_qpsk = r_cp_qpsk(:,33:end);
     
-    % ×ª»»µ½ÆµÓò£¬ÀûÓÃFFT½«OFDM·ûºÅ×ª»»Îª´ı½âµ÷µÄĞÅÏ¢·ûºÅ
+    % è½¬æ¢åˆ°é¢‘åŸŸï¼Œåˆ©ç”¨FFTå°†OFDMç¬¦å·è½¬æ¢ä¸ºå¾…è§£è°ƒçš„ä¿¡æ¯ç¬¦å·
     r_F_bpsk = (sqrt(nBpsk_Ofdm)/nFFT)*fftshift(fft(r_fft_bpsk.')).';
     r_F_qpsk = (sqrt(nBpsk_Ofdm/2)/nFFT)*fftshift(fft(r_fft_qpsk.')).';
     
-    % BPSK ½âµ÷£º½« +value --> 1, ½« -value --> -1
+    % BPSK è§£è°ƒï¼šå°† +value --> 1, å°† -value --> -1
     r_demod_bpsk = real(r_F_bpsk);
     r_demod_bpsk(find(r_demod_bpsk > 0)) = +1;
     r_demod_bpsk(find(r_demod_bpsk < 0)) = -1;    
     r_bit_bpsk = (r_demod_bpsk + 1)/2;
     r_bit_bpsk = reshape(r_bit_bpsk.',nBit_Sym*nSym,1).';
     
-    % QPSK ½âµ÷£ºÊµ¡¢Ğé²¿·Ö±ğBPSK½âµ÷ºóÖØĞÂ×éºÏ
+    % QPSK è§£è°ƒï¼šå®ã€è™šéƒ¨åˆ†åˆ«BPSKè§£è°ƒåé‡æ–°ç»„åˆ
     r_demod_I = real(r_F_qpsk);
     r_demod_Q = imag(r_F_qpsk);
     r_demod_I(find(r_demod_I > 0)) = 1;
@@ -180,21 +179,21 @@ for ii = 1:length(EbN0dB)
     r_demod_Q = reshape(r_demod_Q.', 1, nSym/2*nBit_Sym);
     r_bit_qpsk = reshape([r_demod_I; r_demod_Q],1,nBit_Sym*nSym);
     
-    % ¼ÆËã´íÎóbitsÊıÄ¿
+    % è®¡ç®—é”™è¯¯bitsæ•°ç›®
     nErr_bpsk(ii) = size(find(r_bit_bpsk - s_bit),2);
     nErr_qpsk(ii) = size(find(r_bit_qpsk - s_bit),2);
 end
 
-simBer_bpsk = nErr_bpsk/(nSym*nBit_Sym);                % ·ÂÕæÎó±ÈÌØÂÊ BPSK
-simBer_qpsk = nErr_qpsk/(nSym*nBit_Sym);                % ·ÂÕæÎó±ÈÌØÂÊ QPSK
-theoryBer = (1/2)*erfc(sqrt(10.^(EbN0dB/10)));          % ÀíÂÛÎó±ÈÌØÂÊ BPSK/QPSK
+simBer_bpsk = nErr_bpsk/(nSym*nBit_Sym);                % ä»¿çœŸè¯¯æ¯”ç‰¹ç‡ BPSK
+simBer_qpsk = nErr_qpsk/(nSym*nBit_Sym);                % ä»¿çœŸè¯¯æ¯”ç‰¹ç‡ QPSK
+theoryBer = (1/2)*erfc(sqrt(10.^(EbN0dB/10)));          % ç†è®ºè¯¯æ¯”ç‰¹ç‡ BPSK/QPSK
 % load pro.mat simBer_bpsk theoryBer
 figure
-semilogy(EbN0dB,simBer_bpsk,'--*r','LineWidth',2);      % ·ÂÕæÎó±ÈÌØÂÊ BPSK
+semilogy(EbN0dB,simBer_bpsk,'--*r','LineWidth',2);      % ä»¿çœŸè¯¯æ¯”ç‰¹ç‡ BPSK
 hold on
-semilogy(EbN0dB,simBer_qpsk,'--*g','LineWidth',2);      % ·ÂÕæÎó±ÈÌØÂÊ QPSK
+semilogy(EbN0dB,simBer_qpsk,'--*g','LineWidth',2);      % ä»¿çœŸè¯¯æ¯”ç‰¹ç‡ QPSK
 hold on
-semilogy(EbN0dB,theoryBer,'--*b','LineWidth',2);        % ÀíÂÛÎó±ÈÌØÂÊ BPSK/QPSK
+semilogy(EbN0dB,theoryBer,'--*b','LineWidth',2);        % ç†è®ºè¯¯æ¯”ç‰¹ç‡ BPSK/QPSK
 
 grid on
 legend('BPSK', 'QPSK', 'theory');
